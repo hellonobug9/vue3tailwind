1b1219c25a36d5ca772af4e91927fbb8569b030a
Charlie Tran <CharlieTran@GRAPECITY.NET>
2022-09-22 10:15:38 +0700

    progress

---
+++ src/components/Progress.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-[#E5E5EA] rounded-3xl">
+    <div
+      class="bg-gradient-to-r from-[#FF8008] to-[#FFC837] font-semibold text-end rounded-3xl p-1"
+      :style="`width: ${(complete / total) * 100}%`"
+    >
+      <div
+        class="bg-white rounded-full shadow-2xl flex justify-center items-center w-8 h-8 ml-auto"
+      >
+        {{ complete }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  total: number;
+  complete: number;
+}>();
+</script>
